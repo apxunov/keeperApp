138e5838044e1b4c105777b32b4ceda7f12ddc4c
Mihail Arhipov <arhipov_ma@mail.ru>
2020-10-26 14:41:17 +0300

    notes can be added and removed

---
+++ build/precache-manifest.bd2a3df5da5d2568d668db08e7046f55.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916d61ee34ce06246efca6d2e6f6be6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e00675106ee9764e889",
+    "url": "/static/js/2.a3ab1f42.chunk.js"
+  },
+  {
+    "revision": "3a33263902795c5efa33",
+    "url": "/static/js/main.80e4055e.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
